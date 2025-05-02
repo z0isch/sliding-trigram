5d944f4986dacf7065b6def095c2bc73cf89d684
AJ Ruf <z0isch@gmail.com>
2025-05-01 20:52:44 -0400

    Init

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import AllLetters from "./components/AllLetters";
 import WordHunt from "./components/WordHunt";
 
@@ -8,11 +8,11 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/sliding-trigram">
+    <HashRouter>
       <Routes>
         <Route path="/" element={<AllLetters />} />
         <Route path="/word-hunt" element={<WordHunt />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
